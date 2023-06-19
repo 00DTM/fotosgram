1ab639f0c2b037e1bcffae64873f503cd0b9fedc
Dani Te <daniel.te.mar@gmail.com>
2023-06-19 17:54:46 +0200

    pregunta de encuesta

--- src/app/pages/tab4/tab4.page.ts
+++ src/app/pages/tab4/tab4.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -8,22 +9,45 @@ import { Router } from '@angular/router';
   styleUrls: ['tab4.page.scss'],
 })
 export class Tab4Page {
-  pregunta1: string | undefined;
-  pregunta2: string | undefined;
+  respuesta: string | undefined |null;
+  respuestas: { id: string, respuesta: string }[] = [];
+  mostrarResultados: boolean = false;
+  contadorRespuestas: { diestro: number, zurdo: number } = { diestro: 0, zurdo: 0 };
 
-  constructor(private router: Router) {}
+  constructor() {}
 
-  enviarRespuestas() {
-    let nota = 0;
+  enviarRespuesta() {
+    if (this.respuesta) {
+      const idAleatorio = this.generarIdAleatorio();
+      const respuestaJson = { id: idAleatorio, respuesta: this.respuesta };
+      this.respuestas.push(respuestaJson);
+      this.actualizarContadorRespuestas(this.respuesta);
+      
+      console.log('Respuesta enviada:', respuestaJson);
+      // Aquí puedes realizar acciones adicionales, como enviar el JSON al servidor
 
-    if (this.pregunta1 === 'Sobresaliente') {
-      nota++;
+      // Limpiar la selección de respuesta después de enviar
+      this.respuesta = null;
+    } else {
+      console.log('Por favor, seleccione una opción.');
     }
+  }
 
-    if (this.pregunta2 === 'Daniel') {
-      nota++;
+  mostrarRespuestas() {
+    this.mostrarResultados = true;
+  }
+
+  generarIdAleatorio(): string {
+    // Generar un ID aleatorio utilizando Math.random() y convertirlo a una cadena
+    const id = Math.random().toString(36).substr(2, 9);
+    return id;
+  }
+
+  actualizarContadorRespuestas(respuesta: string) {
+    if (respuesta === 'diestro') {
+      this.contadorRespuestas.diestro++;
+    } else if (respuesta === 'zurdo') {
+      this.contadorRespuestas.zurdo++;
     }
-
-    this.router.navigate(['/resultados'], { queryParams: { nota: nota } });
   }
 }
